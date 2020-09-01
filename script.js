a3705b1dab4d10c7b6db0ad271ca26fb18fbf947
shouhu <mbaazi@mailbox.org>
2020-09-01 18:22:05 +0200

    formatted code

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 const cell = (function createCell() {
-	let squareCell = document.createElement('div');
-	container.appendChild(squareCell);
-	})();
+  let squareCell = document.createElement("div");
+  container.appendChild(squareCell);
+})();
 
 function createGrid() {
-	for(let i = 0; i < 16; i++) {
-		container.cloneNode(cell);
-	};
+  for (let i = 0; i < 16; i++) {
+    container.cloneNode(cell);
+  }
 }
 
 createGrid();
-
