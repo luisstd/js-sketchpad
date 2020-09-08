218ea61c648033b2db65512c886af0243837c173
shouhu <mbaazi@mailbox.org>
2020-09-08 18:05:33 +0200

    added styles and css grid, added eventlistener

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // dom variables
-const container = document.getElementById("container");
+const container = document.querySelector(".container");
+const container__grid = document.querySelector(".container__grid");
 const button = document.querySelector(".button");
 
 // creating the grid
 function createGrid() {
-  for (let i = 0; i < 16; i++) {
-   let cell = document.createElement("div");
-   container.appendChild(cell);
+  for (let i = 0; i < 64; i++) {
+    let cell = document.createElement("div");
+    container__grid.appendChild(cell).classList.add("cell");
+
+    cell.addEventListener(
+      "mouseenter",
+      function (event) {
+        event.target.style.backgroundColor = "black";
+      },
+      false
+    );
   }
 }
 createGrid();
