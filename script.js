3f3199165fff90865334bceacf974ec0c2a533b9
shouhu <mbaazi@mailbox.org>
2020-09-14 16:55:06 +0200

    added reset grid button, added setSize() function

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ const container = document.querySelector(".container");
 const container__grid = document.querySelector(".container__grid");
 const button = document.querySelector(".button");
 
+// set grid size
+let input = parseInt(prompt("Please enter number of squares per side", "Enter any number larger than 16"));
+button.addEventListener("click", setSize());
+
+function setSize() {
+  container__grid.style.setProperty("grid-template-rows", `repeat(${input}, 1fr)`);
+  container__grid.style.setProperty("grid-template-columns", `repeat(${input}, 1fr)`);
+};
+
 // creating the grid
 function createGrid() {
-  for (let i = 0; i < 64; i++) {
+  for (let i = 0; i < input*input ; i++) {
     let cell = document.createElement("div");
     container__grid.appendChild(cell).classList.add("cell");
 
@@ -16,6 +25,6 @@ function createGrid() {
       },
       false
     );
-  }
+  }        
 }
 createGrid();
