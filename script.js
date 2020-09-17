68418df56e7dae3fd53e6a5a26dd7f1c5447040a
shouhu <mbaazi@mailbox.org>
2020-09-17 20:01:56 +0200

    added reset functionality

--- script.js
+++ script.js
@@ -4,38 +4,44 @@ const container__grid = document.querySelector(".container__grid");
 const button = document.querySelector(".button");
 
 // set grid size
-let input = parseInt(
-  prompt(
-    "Please enter number of squares per side",
-    "Enter number between 2 and 100"
-  )
-);
-
-function setSize() {
-  container__grid.style.setProperty(
-    "grid-template-rows",
-    `repeat(${input}, 1fr)`
-  );
-  container__grid.style.setProperty(
-    "grid-template-columns",
-    `repeat(${input}, 1fr)`
-  );
+var input;
+function reset() {
+  window.location.reload();
 }
-setSize();
+function promptSize() {
+  input = parseInt(
+    prompt(
+      "Please enter number of squares per side",
+      "Enter number between 2 and 100"
+    )
+  );
 
-// creating the grid
-function createGrid() {
-  for (let i = 0; i < input * input; i++) {
-    let cell = document.createElement("div");
-    container__grid.appendChild(cell).classList.add("cell");
-
-    cell.addEventListener(
-      "mouseenter",
-      function (event) {
-        event.target.style.backgroundColor = "black";
-      },
-      false
+  function setSize() {
+    container__grid.style.setProperty(
+      "grid-template-rows",
+      `repeat(${input}, 1fr)`
+    );
+    container__grid.style.setProperty(
+      "grid-template-columns",
+      `repeat(${input}, 1fr)`
     );
   }
+
+  function createGrid() {
+    for (let i = 0; i < input * input; i++) {
+      let cell = document.createElement("div");
+      container__grid.appendChild(cell).classList.add("cell");
+
+      cell.addEventListener(
+        "mouseenter",
+        function (event) {
+          event.target.style.backgroundColor = "black";
+        },
+        false
+      );
+    }
+    setSize();
+  }
+
+  createGrid();
 }
-createGrid();
